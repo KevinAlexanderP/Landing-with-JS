0afa56336ea66ff0655efeb06db46b4f67798f62
KevinAlexanderP <kennedyisdead13@gmail.com>
2020-03-15 20:01:15 -0600

    First commit

---
+++ css-project-starter-code/assets/css/Freecodecamp.js
@@ -0,0 +1,13 @@
+// Setup
+var outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+
+var outerWear = "Sweater"
+
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
